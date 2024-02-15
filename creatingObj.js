8e4bfd11b177f89aa3389760a35830b3d83192b3
BIENAIMEE Marie Reine <mbienaimee@alustudent.com>
2024-02-15 11:22:58 +0200

    constructor

--- creatingObj.js
+++ creatingObj.js
@@ -1,19 +1,20 @@
-var emp1 = {};
-emp1.name = 'Marie';
-emp1.gender ='female';
-emp1.position ='Manager';
-emp1.username = emp1.name
-console.log(emp1);
+// var emp1 = {};
+// emp1.name = 'Marie';
+// emp1.gender ='female';
+// emp1.position ='Manager';
+// emp1.username = emp1.name
+// console.log(emp1);
 
 function createEmployeeObject(name,gender,position,username){
-    var emp2={};
-    emp2.name = name;
-    emp2.gender =gender;
-    emp2.position =position;
-    emp2.username = name;
-    return emp2
+    // var emp2={};
+    this.name = name;
+    this.gender =gender;
+    this.position =position;
+    this.username = name;
+    // return emp2
 
 }
-console.log(createEmployeeObject('aimee','female','assistant manager','emp2.name'));
-console.log(createEmployeeObject('kwizera','male','boss','emp3'));
+console.log(new createEmployeeObject('aimee','female','assistant manager','emp2.name'));
+console.log(new createEmployeeObject('kwizera','male','boss','emp3'));
+console.log(new createEmployeeObject('Yvonne','female','secreatary','emp3'));
 
